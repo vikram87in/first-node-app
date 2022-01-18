2fffb390b87dcb0d5f308cd19c2f12e913fba29c
vikram singh <vikram.singh@bold.com>
2022-01-18 21:26:00 +0530

    updated commonJS code to ESModule code

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const path = require('path');
-const process = require('process');
+import express from 'express';
+import { cwd } from 'process';
+import ejs from 'ejs';
+import { fName, fetchData, fetchDataWithAxios } from './javascript/file1.js';
+
 
 const port = 3000;
 const app = express();
 
 //sets the path for the static files in two different ways
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(`${process.cwd()}/javascript`));
+app.use(express.static(`${cwd()}/public`));
+app.use(express.static(`${cwd()}/javascript`));
 
 // sets the default extension
 app.set('view engine', 'html');
 
 //  To be able to use .html file instead of .ejs file, use any one of the below lines
-// app.engine('html', (require('ejs').__express));
-app.engine('html', require('ejs').renderFile)
+app.engine('html', ejs.renderFile)
 
 // sets the default folder to anything other than '/views' to find the file to render
 // app.set('views', process.cwd() + '/views');
 
 app.get('/', (req, res) => {
-    console.log('from index.js get');
-    var { fName, fetchData, fetchDataWithAxios } = require('./javascript/file1');
     // fetchData()
     fetchDataWithAxios()
         .then(data => {
             res.render('index', { text: 'World', title: 'Node Practice', data, fName });
-        });
+        })
+        .catch(err => res.render('index', {
+            error: true, message: 'Sorry, the data could not be fetched',
+            errorMsg: err, fName, title: 'Node Practice'
+        }));
 });
 
 app.listen(port, () => {
--- javascript/file1.js
+++ javascript/file1.js
@@ -1,26 +1,25 @@
-console.log('loaded from file1');
-const path = 'https://api-qa-embedded-builder.myperfectcoverletter.com/api/v1/config/features/mpc?skipCache=true&includeInActiveFeatures=true';
-const fetchData = () => {
-    const fetch = require('node-fetch');
-    return fetch(path)
-        .then(res => res.json())
-        .then(data => {
-            // console.log(data);
-            return data;
-        })
-        .catch(err => console.log(err));
+import axios from 'axios';
+import fetch from 'node-fetch';
+
+const path = 'https://jsonplaceholder.typicode.com/posts/1/comments';
+export const fetchData = async () => {
+    try {
+        const res = await fetch(path);
+        const data = res.json();
+        return data;
+    } catch (err) {
+        throw `fetch error is: ${err.message || err}`;
+    }
 }
 
-const fetchDataWithAxios = () => {
-    const axios = require('axios').default;
-    return axios.get(path)
-        .then(data => {
-            // console.log(data.data);
-            return data.data;
-        })
-        .catch(err => console.log(err));
+export const fetchDataWithAxios = async () => {
+    try {
+        const data = await axios.get(path);
+        return data.data;
+    } catch (err) {
+        throw `axios error is: ${err.message || err}`;
+    }
 }
 
-const fName = 'Vikram';
+export const fName = 'Vikram';
 
-module.exports = { fetchData, fName, fetchDataWithAxios };
